47b2a717c139732112332617e5eea2edd75f5ee4
hmtmcse <hmtm.cse@gmail.com>
2023-10-26 10:30:08 +0600

    Added External Export

--- pweb_orm/__init__.py
+++ pweb_orm/__init__.py
@@ -2,7 +2,7 @@ from pweb_orm.orm.pweb_orm import pweb_orm
 from pweb_orm.orm.pweb_saas import PWebSaaS
 from pweb_orm.orm.pweb_saas import PWebSaaSTenantResolver
 from pweb_orm.model.pweb_model import PWebBaseModel
-from pweb_orm.model.pweb_model import PWebRelationalModel
+from pweb_orm.model.pweb_model import PWebRelationalModel, PWebSingleModel
 from pweb_orm.model.pweb_model import PwebModel
 from pweb_orm.model.pweb_model import PWebABCModel
 from pweb_orm.common.pweb_orm_util import PWebORMUtil
