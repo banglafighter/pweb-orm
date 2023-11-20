dab08b125b8db73bafbf87c15d820db73a75a2a8
hmtmcse <hmtm.cse@gmail.com>
2023-11-20 07:27:55 +0600

    Beta Release Version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_dependencies():
 
 setup(
     name='pweb-orm',
-    version='0.0.3',
+    version='0.0.4',
     url='https://github.com/banglafighter/pweb-orm',
     license='Apache 2.0',
     author='Bangla Fighter',
